a0abdb095d9d987e627160c5c148b3fd38f0d821
Mark Van houtert <mvan-hou@f1r6s10.codam.nl>
2022-03-07 13:45:43 +0100

    use enum for error type

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as Error from './error'
 
 (() => {
     if (process.argv.length < 3)
-        Error.handle(0);
+        Error.handle(Error.type.NOT_ENOUGH_ARGUMENTS);
     const parser = new Parser(process.argv[2]);
     console.log(parser.parse());
 })();
