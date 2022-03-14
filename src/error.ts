2775553c52780f2a64a11eb34e68e4a852462459
Mark Van houtert <mvan-hou@f1r6s9.codam.nl>
2022-03-14 17:25:09 +0100

    error printing

--- src/error.ts
+++ src/error.ts
@@ -1,24 +1,30 @@
 export enum type {
     NOT_ENOUGH_ARGUMENTS,
-    UNEXPECTED_END_OF_INPUT
+    UNEXPECTED_END_OF_INPUT,
+    INCORRECT_NUMBER
 }
 
 export class ErrorContext {
-    _cursor:number
-    _contextString:string
+    cursor:number
+    contextString:string
 
     constructor(contextString:string, cursor:number=-1) {
-        this._contextString = contextString
-        this._cursor = cursor
+        this.contextString = contextString
+        this.cursor = cursor
     }
 }
+
+const contextString = (context:ErrorContext) => `${context.contextString} at position ${context.cursor}`;
 
 export const handle = (code:number, context:ErrorContext|null=null) => {
     const errorPrefix:string = "[ERROR]"
     const errors:string[] = [
         'Not enough arguments',
-        'Unexpected end of input'
+        'Unexpected end of input',
+        'Incorrect numeric literal'
     ];
     console.log(errorPrefix, errors[code]);
+    if (context)
+        console.log(contextString(context));
     process.exit();
 }
